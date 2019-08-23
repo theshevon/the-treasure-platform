81ed1812936ead990269886c1036daf814f5fbd3
Shevon Mendis <shevonmendis98@gmail.com>
2019-08-23 22:27:20 +1000

    linked to firebase

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import fire from './config/fire';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // called for any authentication changes
+  authListener(){
+    fire.auth().onAuthStageChanged((user) => {
+      if (user){
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
