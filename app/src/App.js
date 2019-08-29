30b549c4658245e782837314a5619f6a3b925adc
Shevon Mendis <shevonmendis98@gmail.com>
2019-08-29 23:52:57 +1000

    integrated express backend

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg'
-import fire from './config/fire';
-import "./App.css"
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
+  state = {
+    data: null
+  };
 
-  render(){
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        // Render the newly fetched data inside of this.state.data
+        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
