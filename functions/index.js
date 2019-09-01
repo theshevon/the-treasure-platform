9fc5526bb9d2b11efa4d6b35d5e29ca735ef207c
Shevon Mendis <shevonmendis98@gmail.com>
2019-09-01 11:49:05 +1000

    register route

--- functions/index.js
+++ functions/index.js
@@ -30,7 +30,47 @@ const db = admin.firestore();
 
 // registration route
 app.post("/register", (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    }
 
+    db.doc(`/users/${ newUser.handle }`)
+        .get()
+        .then(doc => {
+            if (doc.exists){
+                return res
+                        .status(400)
+                        .json({
+                                handle: "this handle is already taken"
+                              });
+            } else {
+                return firebase
+                        .auth
+                        .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+
+        // the new user has been created so return an access/ auth token so that
+        // the user will be able to use it to request more data
+        .then(data => {
+            return data.user.getIdToken();
+        })
+
+        .then(token => {
+            return res.status(201).json({ token });
+        })
+
+        .catch(err => {
+            console.error(err);
+            if (err.code === "auth/email-already-in-use"){
+                return res.status(400).json({ email: "Email is already in use" });
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        })
 });
 
 // all items GET route
