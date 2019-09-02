cc1ddab9158a02e100a69cf89e67ed9934894aa5
Shevon Mendis <shevonmendis98@gmail.com>
2019-09-03 04:51:44 +1000

    basic route set-up

---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default home
