5c7b9be576d009795061687e132a921752c898ca
Shevon Mendis <shevonmendis98@gmail.com>
2019-09-08 15:53:44 +1000

    adjusted for stub data abstraction

---
+++ client/src/data/items.js
@@ -0,0 +1,21 @@
+var items = [
+            {
+                name: "Dragoon G Beyblade",
+                description: "Was owned by my grandpa, 'Buffalo Bill'"
+            },
+            {
+                name: "item2",
+                description: "item2 desc"
+            },
+            {
+                name: "item3",
+                description: "item3 desc"
+            },
+            {
+                name: "item4",
+                description: "item4 desc"
+            }
+]
+
+export default items;
+
