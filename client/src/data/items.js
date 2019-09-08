da70f7743ec67c56373cb105b98d545c3fd85b7e
Shevon Mendis <shevonmendis98@gmail.com>
2019-09-08 16:47:19 +1000

    item stub data amended

--- client/src/data/items.js
+++ client/src/data/items.js
@@ -5,7 +5,7 @@
  * name      - string      // item name
  * desc      - string      // item description
  * img_src   - string      // URI for item image
- * val       - string      // value (optional)
+ * val       - string      // val (optional)
  * createdon - timestamp   // time of account creation
  * visibleto - [users]     // list of users who the item is visible to
  * intusers  - [users]     // list of users who've expressed interest in the item
@@ -17,8 +17,8 @@ var items = [
         name: "item1",
         desc: "Was owned by my grandpa, 'Buffalo Bill'",
         img_src: "https://images.unsplash.com/photo-1511253819057-5408d4d70465?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        value: null,
-        addedon: "September 1, 2019 at 12:10:00 AM UTC+10",
+        val: null,
+        createdon: "September 1, 2019 at 12:10:00 AM UTC+10",
         visibleto: [
                         "user1",
                         "user2"
@@ -30,8 +30,8 @@ var items = [
         name: "item2",
         desc: "item2 desc",
         img_src: "https://images.unsplash.com/photo-1511253819057-5408d4d70465?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        value: null,
-        addedon: "September 1, 2019 at 12:10:00 AM UTC+10",
+        val: null,
+        createdon: "September 1, 2019 at 12:10:00 AM UTC+10",
         visibleto: [
                         "user1",
                         "user2"
@@ -43,8 +43,8 @@ var items = [
         name: "item3",
         desc: "item3 desc",
         img_src: "https://images.unsplash.com/photo-1511253819057-5408d4d70465?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        value: null,
-        addedon: "September 1, 2019 at 12:10:00 AM UTC+10",
+        val: null,
+        createdon: "September 1, 2019 at 12:10:00 AM UTC+10",
         visibleto: [
                         "user1",
                         "user2"
@@ -56,8 +56,8 @@ var items = [
         name: "item4",
         desc: "item4 desc",
         img_src: "https://images.unsplash.com/photo-1511253819057-5408d4d70465?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        value: null,
-        addedon: "September 1, 2019 at 12:10:00 AM UTC+10",
+        val: null,
+        createdon: "September 1, 2019 at 12:10:00 AM UTC+10",
         visibleto: [
                         "user1",
                         "user2"
