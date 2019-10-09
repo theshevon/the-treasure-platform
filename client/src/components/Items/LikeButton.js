3a2c56261948f5995657f2d4d4b94c73af40ee72
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-09 16:17:38 +1100

    added axios request

--- client/src/components/Items/LikeButton.js
+++ client/src/components/Items/LikeButton.js
@@ -1,12 +1,13 @@
 // react imports
 import React, { Component } from "react"
+import axios from 'axios'
 
 // custom stylesheets
 import "../../stylesheets/item.css"
 
 // custom icons
 import likedBtn   from "../../icons/liked.svg"
-import unlikedBtn from "../../icons/unliked.svg";
+import unlikedBtn from "../../icons/unliked.svg"
 
 // A heart button that toggles between filled heart and outlined heart
 class LikeButton extends Component {
@@ -15,12 +16,25 @@ class LikeButton extends Component {
 		liked: false,
 	}
 
-	// TODO:
-	// Integration: send axios request to up
 	handleClick = () => {
 		this.setState({
 						liked: !this.state.liked
 					});
+
+		//  send update request to server
+		// let route = this.state.liked ? "int" : "notint";
+		// axios({
+		// 		method: 'post',
+		// 		url: `http://localhost:5000/comp30022app/us-central1/api/items/${this.props.itemID}/${route}/${this.userId}`,
+		// 	  })
+		// 	  .then(res => {
+		// 		  // do nothing with response
+		// 		  return
+		// 	  })
+		// 	  .catch(err => {
+		// 		  // if there was an error, simply reverse the user's action
+		// 		  this.handleClick();
+		// 	  })
 	}
 
 	render() {
