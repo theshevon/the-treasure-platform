675a81f32eae7a512823b348121ec441dda3c1d1
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-16 19:18:43 +1100

    removed recaptcha and finalised ui

--- client/src/pages/support.js
+++ client/src/pages/support.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios                from 'axios';
 
 // bootstrap imports
-import Spinner from 'react-bootstrap/Spinner'
-import Button  from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import Row  from 'react-bootstrap/Row'
-import Col  from 'react-bootstrap/Col'
-
-import axios from 'axios'
-
-// other imports
-import ReCAPTCHA from "react-google-recaptcha";
-
-// ReCaptcha keys
-const SITE_KEY = "6Ld1er0UAAAAAONvG_XKNAPoMFYkts3_aIaqJslk"
+import Spinner from 'react-bootstrap/Spinner';
+import Button  from 'react-bootstrap/Button';
+import Alert   from 'react-bootstrap/Alert';
+import Form    from 'react-bootstrap/Form';
+import Row     from 'react-bootstrap/Row';
+import Col     from 'react-bootstrap/Col';
 
 class support extends Component {
 
     state = {
-        loading : false,
-        topic   : '',
-        message : '',
-        recaptchaToken: ''
-    }
-
-    onCaptchaChange = (value) => {
-      console.log("Captcha value:", value);
-      this.setState({recaptchaToken : value});
+        loading   : false,
+        subject   : '',
+        message   : '',
+        showAlert : false,
+        alertMsg  : ''
     }
 
     handleChange = event => {
@@ -40,22 +30,22 @@ class support extends Component {
         this.setState({ loading : true });
 
         let data = {
-            recaptchaToken: this.state.recaptchaToken,
-            topic   : this.state.topic,
-            message : this.state.message
+            subject        : this.state.subject,
+            message        : this.state.message
         }
 
         axios({
                 method : 'post',
-                url    : 'http://localhost:5000/comp30022app/us-central1/api/support',
-                data   : data
+                url    : '/support',
+                data   :  data
             })
             .then((res) => {
                 this.setState({
-                                loading  : false,
-                                topic    : '',
-                                message  : '',
-                                response : res.data()
+                                loading   : false,
+                                topic     : '',
+                                message   : '',
+                                showAlert : true,
+                                alertMsg  : "Thanks for you message! Someone from our team will get back to you shortly!"
                             });
                 return
             })
@@ -66,80 +56,67 @@ class support extends Component {
 
 
     render() {
-          let btnContent;
-              if (this.state.loading){
-                  btnContent = (<Spinner animation="border" size="sm"/>);
-              } else {
-                  btnContent = ("Send");
-              }
 
-              return (
-                  <div
-      				className="main-container"
-      				style={{width:"100vw", height:"100vh"}}>
-      				<Row
-      					className="login-form-container d-flex justify-content-center">
-      					<Col
-      						className="login-form-body p-5"
-      						xs="10"
-      						sm="6"
-      						md="3">
-      						<h1
-      							className="form-title mb-4">
-      							Tell Us What's Wrong
-      						</h1>
-      						<Form
-      							noValidate
-      							validated={this.state.validated}
-      							onSubmit={this.handleSubmit}>
-      							<Row
-      								className="my-1">
-      								<Form.Control
-      									className="login-field"
-      									name="topic"
-      									placeholder="topic"
-      									value={this.state.topic}
-      									onChange={this.handleChange}
-      									required/>
-      								<Form.Control.Feedback
-      									type="invalid">
+        let btnContent = ("Send");
+        if (this.state.loading){
+            btnContent = (<Spinner animation="border" size="sm"/>);
+        }
 
-                  					</Form.Control.Feedback>
-      							</Row>
-      							<Row
-      								className="my-1">
-      								<Form.Control
-                                          as="textarea"
-                                          name="desc"
-                                          rows="5"
-                                          placeholder="message"
-                                          required
-                                          onChange={this.handleChange}/>
-      								<Form.Control.Feedback
-      									type="invalid">
-                  					</Form.Control.Feedback>
-      							</Row>
-                                <div
-                                    class="d-flex justify-content-center">
+        return (
 
-                                <ReCAPTCHA
-                                    sitekey={SITE_KEY}
-                                    onChange={this.onCaptchaChange}
-                                />
-
-                                </div>
-      							<Button
-      								className="login-btn btn mt-3"
-      								type="submit"
-      								onClick={this.handleSubmit}
-                                      disabled={this.state.loading}>
-                                      {btnContent}
-                                  </Button>
-      						</Form>
-      					</Col>
-      				</Row>
-      			</div>
-            )
+            <div
+                className="main-container cover-div"
+                style={{width:"100vw", height:"100vh"}}>
+                <Row
+                    className="login-form-container d-flex justify-content-center">
+                    <Col
+                        className="login-form-body p-5"
+                        xs="10"
+                        sm="6"
+                        md="4">
+                        <h1
+                            className="form-title mb-2">
+                            Tell Us What's Wrong
+                        </h1>
+                        <p
+                            className="text-muted text-center mb-4">
+                            Experiencing issues when trying to do something?<br/>Let us know and we'll work on fixing it right away!
+                        </p>
+                        <Form
+                            onSubmit={this.handleSubmit}>
+                            <Row
+                                className="my-1">
+                                <Form.Control
+                                    className="login-field"
+                                    name="topic"
+                                    placeholder="topic"
+                                    value={this.state.topic}
+                                    onChange={this.handleChange}
+                                    required/>
+                            </Row>
+                            <Row
+                                className="my-1">
+                                <Form.Control
+                                        as="textarea"
+                                        name="desc"
+                                        rows="5"
+                                        placeholder="message"
+                                        required
+                                        onChange={this.handleChange}/>
+                            </Row>
+                            <Button
+                                className="btn centered-btn mt-3"
+                                variant="light"
+                                type="submit"
+                                onClick={this.handleSubmit}
+                                disabled={this.state.loading}>
+                                {btnContent}
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
+        )
     }
 }
 
