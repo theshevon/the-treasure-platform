47511f94efbf3429feb3e3cd4cc9efdda783a979
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-17 19:50:22 +1100

    fixed logout issue

--- client/src/components/util/PrivateRoute.js
+++ client/src/components/util/PrivateRoute.js
@@ -6,11 +6,19 @@ import PropTypes           from 'prop-types';
 const PrivateRoute = ({ component: Component, authenticated, ...rest }) => (
 	<Route
 		{...rest}
-		render={props =>
-					(authenticated === true && parseInt(localStorage.TreasureUType) === 0)
-					? <Component {...props}/>
-					: <Redirect
-						to='/error'/> // redirects to error page
+		render={props => {
+							if (authenticated === true){
+								if (parseInt(localStorage.TreasureUType) === 0){
+									return <Component {...props}/>;
+								} else {
+									return <Redirect
+												to='/error'/>;
+								}
+							} else {
+								return <Redirect
+											to='/login'/>;
+							}
+						}
 				}
 	/>
 );
