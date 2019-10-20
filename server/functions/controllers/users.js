c8cb8d45cb76ebe6affcf85db25201fd59dd9422
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-21 00:49:28 +1100

    updated registration procedure on backend

--- server/functions/controllers/users.js
+++ server/functions/controllers/users.js
@@ -87,7 +87,7 @@ exports.registerNewUser =
         firebase
             .auth()
             .createUserWithEmailAndPassword(newUser.email, newUser.pw)
-            .then(data => {
+            .then(async data => {
 
                 let uid = data.user.uid;
 
@@ -102,43 +102,44 @@ exports.registerNewUser =
                     imgSrc: newUser.imgSrc
                 }
 
-                // eslint-disable-next-line promise/no-nesting
-                return db
+                try {
+                    await db
                         .collection("users")
                         .doc(uid)
-                        .set(userData)
-                        .then(() => {
-                            return res.status(200).json(uid);
-                        })
-                        .catch(err => {
-                            return res.status(400).json({ error: err });
-                        })
+                        .set(userData);
+                    return { email: userData.email };
+                }
+                catch (err) {
+                    console.log(err);
+                    return res.status(400).json({ general: "Sorry, something went wrong." });
+                }
 
             })
+            .then(async data => {
+                try {
+                    await db
+                        .collection('invitees')
+                        .doc(data.email)
+                        .update({ accepted: true });
+                    return res.status(200).json("Success: User registered");
+                }
+                catch (err) {
+                    console.log(err);
+                    return res.status(400).json({ general: "Sorry, something went wrong." });
+                }
+            })
             .catch(err => {
+                console.log(err);
                 if (err.code === "auth/email-already-in-use"){
                     return res.status(400).json({ email: "Email is already in use" });
-                } else {
-                    return res.status(400).json({ error: err.code });
+                }
+                else if (err.code === "auth/weak-password"){
+                    return res.status(400).json({ pw : err.message });
+                }
+                else {
+                    return res.status(400).json({ general: "Sorry, something went wrong." });
                 }
             });
-
-            // // change accepted field to true
-            // invitee.accepted = true;
-
-            // // eslint-disable-next-line promise/no-nesting
-            // return db
-            //         .collection('invitees')
-            //         .doc(invitee.email)
-            //         .set(invitee)
-            //         .then(() => {
-            //             return res.status(200).json("Success: User validated");
-            //         })
-            //         .catch(err => {
-            //             console.log(err);
-            //             return res.status(400).json({ general : "Sorry, something went wrong." });
-            //         });
-
     }
 
 exports.logInUser =
