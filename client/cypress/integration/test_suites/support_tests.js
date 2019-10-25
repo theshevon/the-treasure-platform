c70472e0a95d3c3af5c74923defbcd9c8c4500d3
Michael Manoussakis <mihalis.mana@gmail.com>
2019-10-25 18:48:59 +1100

    added support message tests

--- client/cypress/integration/test_suites/support_tests.js
+++ client/cypress/integration/test_suites/support_tests.js
@@ -175,4 +175,88 @@ describe("Support Tests", function() {
 
     });
 
+
+    it.only("Support request: Empty subject, valid message", function() {
+
+        // visit the log in page
+        cy.visit("http://localhost:3000/login");
+
+        // type in a valid email
+        cy
+            .get('input[name="email"]')
+            .type('pu@test.com');
+
+        // type in an invalid password
+        cy
+            .get('input[name="password"]')
+            .type('password');
+
+        // click on the log in button
+        cy
+            .contains('Log In')
+            .click();
+
+        // wait for the next page to load
+        cy
+            .wait(2000);
+
+        // ensure that the page is now the items page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/items');
+
+        // click support from the Navbar
+        cy
+            .get('.dropdown-toggle')
+            .click()
+            .get('.dropdown-item')
+            .eq(0)
+            .click();
+
+        // wait for the next page to load
+        cy
+            .wait(2000);
+
+        // ensure that the page is now the support page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/support');
+
+        cy
+            .get('textarea[name="message"]')
+            .type('test message');
+
+        // click on the send button
+        cy
+            .contains('Send')
+            .click();
+
+        // ensure that the page is still the same
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/support');
+
+        // ensure that the subject field has a red border
+        cy
+            .get('input[name="subject"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that the message field does not have a red border
+        cy
+            .get('textarea[name="message"]')
+            .should('not.have.class', 'invalid-field');
+
+        // ensure that the error feedback for a blank subject is present
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Please enter a subject!');
+
+        // ensure that there is no error message for the message field
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(1)
+            .should('not.exist');
+    });
+
 });
