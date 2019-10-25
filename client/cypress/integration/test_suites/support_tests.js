67115c4e9d5cd61a415c352d2adf37a61aced3b4
Michael Manoussakis <mihalis.mana@gmail.com>
2019-10-25 18:33:34 +1100

    added support test for empty inputs

--- client/cypress/integration/test_suites/support_tests.js
+++ client/cypress/integration/test_suites/support_tests.js
@@ -26,16 +26,6 @@ describe("Support Tests", function() {
             .url()
             .should('eq', 'http://localhost:3000/items');
 
-        // check local storage values
-        cy
-            .get('window')
-            .should(() => {
-                expect(localStorage.getItem('TreasureIDToken')).to.not.eq(null);
-                expect(localStorage.getItem('TreasureUType')).to.eq('0');
-                expect(localStorage.getItem('TreasureUName')).to.not.eq(null);
-                expect(localStorage.getItem('TreasureUImg')).to.not.eq(null);
-            });
-
         // click support from the Navbar
         cy
             .get('.dropdown-toggle')
@@ -85,15 +75,54 @@ describe("Support Tests", function() {
             .url()
             .should('eq', 'http://localhost:3000/items');
 
-        // check local storage values
+        // click support from the Navbar
         cy
-            .get('window')
-            .should(() => {
-                expect(localStorage.getItem('TreasureIDToken')).to.not.eq(null);
-                expect(localStorage.getItem('TreasureUType')).to.eq('1');
-                expect(localStorage.getItem('TreasureUName')).to.not.eq(null);
-                expect(localStorage.getItem('TreasureUImg')).to.not.eq(null);
-            });
+            .get('.dropdown-toggle')
+            .click()
+            .get('.dropdown-item')
+            .eq(0)
+            .click();
+
+        // wait for the next page to load
+        cy
+            .wait(2000);
+
+        // ensure that the page is now the support page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/support');
+
+    });
+
+
+    it("Support request: Empty subject, empty message", function() {
+
+        // visit the log in page
+        cy.visit("http://localhost:3000/login");
+
+        // type in a valid email
+        cy
+            .get('input[name="email"]')
+            .type('pu@test.com');
+
+        // type in an invalid password
+        cy
+            .get('input[name="password"]')
+            .type('password');
+
+        // click on the log in button
+        cy
+            .contains('Log In')
+            .click();
+
+        // wait for the next page to load
+        cy
+            .wait(2000);
+
+        // ensure that the page is now the items page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/items');
 
         // click support from the Navbar
         cy
@@ -111,6 +140,38 @@ describe("Support Tests", function() {
         cy
             .url()
             .should('eq', 'http://localhost:3000/support');
+
+        // click on the send button
+        cy
+            .contains('Send')
+            .click();
+
+        // ensure that the page is still the same
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/support');
+
+        // ensure that the subject field has a red border
+        cy
+            .get('input[name="subject"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that the message field has a red border
+        cy
+            .get('textarea[name="message"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that the error feedback for a blank subject is present
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Please enter a subject!');
+
+        // ensure that the error feedback for a blank message is present
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(1)
+            .should('have.text', 'Please enter a message!');
 
     });
 
