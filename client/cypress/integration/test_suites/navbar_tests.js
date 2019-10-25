4e3e83830e039232bfab0206be6785ec1c54b53b
Michael Manoussakis <mihalis.mana@gmail.com>
2019-10-25 16:43:01 +1100

    navbar tests expanded

--- client/cypress/integration/test_suites/navbar_tests.js
+++ client/cypress/integration/test_suites/navbar_tests.js
@@ -51,10 +51,6 @@ describe("Navbar Tests", function() {
     });
 
 
-
-
-
-
     it("Go to Dashboard page from Items page (Primary User)", function() {
 
         // visit the log in page
@@ -112,9 +108,6 @@ describe("Navbar Tests", function() {
             .should('eq', 'http://localhost:3000/dashboard');
 
     });
-
-
-
 
 
     it("View Navbar from Dashboard page (Primary User)", function() {
@@ -188,9 +181,7 @@ describe("Navbar Tests", function() {
     });
 
 
-
-
-    it("Go to Items page (Primary User)", function() {
+    it("Reload Items page via Navbar (Primary User)", function() {
 
         // visit the log in page
         cy.visit("http://localhost:3000/login");
@@ -247,8 +238,6 @@ describe("Navbar Tests", function() {
             .should('eq', 'http://localhost:3000/items');
 
     });
-
-
 
 
     it("Go to Items page from Dashboard page (Primary User)", function() {
@@ -327,6 +316,86 @@ describe("Navbar Tests", function() {
         cy
             .url()
             .should('eq', 'http://localhost:3000/items');
+
+    });
+
+
+    it("Reload Dashboard page via Navbar (Primary User)", function() {
+
+        // visit the log in page
+        cy.visit("http://localhost:3000/login");
+
+        // type in a valid email
+        cy
+            .get('input[name="email"]')
+            .type('pu@test.com');
+
+        // type in an invalid password
+        cy
+            .get('input[name="password"]')
+            .type('password');
+
+        // click on the log in button
+        cy
+            .contains('Log In')
+            .click();
+
+        // ensure that the page is now the items page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/items');
+
+        // check local storage values
+        cy
+            .get('window')
+            .should(() => {
+                expect(localStorage.getItem('TreasureIDToken')).to.not.eq(null);
+                expect(localStorage.getItem('TreasureUType')).to.eq('0');
+                expect(localStorage.getItem('TreasureUName')).to.not.eq(null);
+                expect(localStorage.getItem('TreasureUImg')).to.not.eq(null);
+            });
+
+        // check navbar has dashboard
+        cy
+            .get('.nav-link')
+            .eq(0)
+            .should('have.text', 'Dashboard');
+
+        // click dashboard from the navbar
+        cy
+            .get('.nav-link')
+            .eq(0)
+            .click()
+
+        // wait for the next page to load
+        cy
+            .wait(3000);
+
+        // ensure that the page is now the dashboard page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/dashboard');
+
+        // check navbar has dashboard
+        cy
+            .get('.nav-link')
+            .eq(0)
+            .should('have.text', 'Dashboard');
+
+        // click dashboard from the navbar
+        cy
+            .get('.nav-link')
+            .eq(0)
+            .click()
+
+        // wait for the next page to load
+        cy
+            .wait(3000);
+
+        // ensure that the page is now the dashboard page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/dashboard');
 
     });
 
