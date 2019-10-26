af0df226131140924420c92f5cd3f2d502b44720
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-26 18:01:09 +1100

    finalised registration tests

--- client/cypress/integration/test_suites/registration_tests.js
+++ client/cypress/integration/test_suites/registration_tests.js
@@ -236,6 +236,48 @@ describe("Stage 1 Tests", () => {
             .should('have.text', 'Sorry, the email address or code you entered was incorrect.');
     });
 
+    it("Email field - valid, Code field - valid (Registered User)", () => {
+
+        // visit the register page
+        cy.visit('http://localhost:3000/register');
+
+        // type in an invalid email address
+        cy
+            .get('input[name="email"]')
+            .type('su@test.com');
+
+        // type in a code
+        cy
+            .get('input[name="code"]')
+            .type(Math.random().toString(36).substring(2, 10));
+
+        // click on the next button
+        cy
+            .contains('Next')
+            .click();
+
+        // ensure that the page is still the same
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/register');
+
+        // ensure that the email field has a red border
+        cy
+            .get('input[name="email"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that the code field has a red border
+        cy
+            .get('input[name="code"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that the error feedback is present
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Error: This invitation has already been accepted.');
+    });
+
     it("Email field - valid, Code field - valid (Non-Invited User)", () => {
 
         // visit the register page
@@ -280,33 +322,40 @@ describe("Stage 1 Tests", () => {
 
 });
 
-describe("Stage 2 Tests", () => {
+var validInviteeEmail = 'invited_user@test.com';
+
+Cypress.Commands.add('do_stage_1', () => {
+
+    // visit the register page
+    cy.visit('http://localhost:3000/register');
+
+    // type in an invalid email address
+    cy
+        .get('input[name="email"]')
+        .type(validInviteeEmail);
+
+    // type in a code
+    cy
+        .get('input[name="code"]')
+        .type('1bg2vxa4');
+
+    // click on the next button
+    cy
+        .contains('Next')
+        .click();
+
+    // wait for validation
+    cy
+        .wait(5000);
+});
+
+describe.only("Stage 2 Tests", () => {
+
+    beforeEach(() => {
+        cy.do_stage_1()
+    })
 
     it("Check if the necessary elements are present", () => {
-
-        let email = 'invited_user@test.com';
-
-        // visit the register page
-        cy.visit('http://localhost:3000/register');
-
-        // type in an invalid email address
-        cy
-            .get('input[name="email"]')
-            .type(email);
-
-        // type in a code
-        cy
-            .get('input[name="code"]')
-            .type('1bg2vxa4');
-
-        // click on the next button
-        cy
-            .contains('Next')
-            .click();
-
-        // wait for validation
-        cy
-            .wait(3000);
 
         // check if profile pic button present
         cy
@@ -330,7 +379,7 @@ describe("Stage 2 Tests", () => {
         cy
             .get('input[name="email"]')
             .should('exist')
-            .should('have.value', email)
+            .should('have.value', validInviteeEmail)
             .should('have.attr', 'readonly')
 
         // check if password field present
@@ -350,4 +399,221 @@ describe("Stage 2 Tests", () => {
             .should('exist')
             .should('have.text', 'Register');
     });
+
+    it("All blank fields", () => {
+
+        // click register button
+        cy
+            .get('.btn')
+            .eq(1)
+            .click()
+
+        // wait for validation
+        cy
+            .wait(5000);
+
+        // ensure that invalid fields have red borders
+
+        cy
+            .get('input[name="fname"]')
+            .should('have.class', 'invalid-field');
+
+        cy
+            .get('input[name="lname"]')
+            .should('have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw"]')
+            .should('have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw_c"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that invalid fields have appropriate error messages
+
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Please enter your first name!');
+
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(1)
+            .should('have.text', 'Please enter your last name!');
+
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(2)
+            .should('have.text', 'Please enter a password!');
+    });
+
+    it("Short password", () => {
+
+        // enter data
+
+        cy
+            .get('input[name="fname"]')
+            .type('First');
+
+        cy
+            .get('input[name="lname"]')
+            .type('Last');
+
+        cy
+            .get('input[name="pw"]')
+            .type('short');
+
+        cy
+            .get('input[name="pw_c"]')
+            .type('short');
+
+        // click register button
+        cy
+            .get('.btn')
+            .eq(1)
+            .click()
+
+        // wait for validation
+        cy
+            .wait(5000);
+
+        // ensure that invalid fields have red borders
+
+        cy
+            .get('input[name="fname"]')
+            .should('not.have.class', 'invalid-field');
+
+        cy
+            .get('input[name="lname"]')
+            .should('not.have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw"]')
+            .should('have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw_c"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that invalid fields have appropriate error messages
+
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Password should be at least 6 characters');
+
+        // ensure that password fields are cleared
+
+        cy
+            .get('input[name="pw"]')
+            .should('have.value', '');
+
+        cy
+            .get('input[name="pw_c"]')
+            .should('have.value', '');
+    });
+
+    it("Password mismatch", () => {
+
+        // enter data
+
+        cy
+            .get('input[name="fname"]')
+            .type('First');
+
+        cy
+            .get('input[name="lname"]')
+            .type('Last');
+
+        cy
+            .get('input[name="pw"]')
+            .type('password');
+
+        cy
+            .get('input[name="pw_c"]')
+            .type('paddword');
+
+        // click register button
+        cy
+            .get('.btn')
+            .eq(1)
+            .click()
+
+        // wait for validation
+        cy
+            .wait(5000);
+
+        // ensure that invalid fields have red borders
+
+        cy
+            .get('input[name="fname"]')
+            .should('not.have.class', 'invalid-field');
+
+        cy
+            .get('input[name="lname"]')
+            .should('not.have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw"]')
+            .should('have.class', 'invalid-field');
+
+        cy
+            .get('input[name="pw_c"]')
+            .should('have.class', 'invalid-field');
+
+        // ensure that invalid fields have appropriate error messages
+
+        cy
+            .get('.invalid-feedback-msg')
+            .eq(0)
+            .should('have.text', 'Passwords must match!');
+
+        // ensure that password fields are cleared
+
+        cy
+            .get('input[name="pw"]')
+            .should('have.value', '');
+
+        cy
+            .get('input[name="pw_c"]')
+            .should('have.value', '');
+    });
+
+    it("All valid data", () => {
+
+        // enter data
+
+        cy
+            .get('input[name="fname"]')
+            .type('First');
+
+        cy
+            .get('input[name="lname"]')
+            .type('Last');
+
+        cy
+            .get('input[name="pw"]')
+            .type('password');
+
+        cy
+            .get('input[name="pw_c"]')
+            .type('password');
+
+        // click register button
+        cy
+            .get('.btn')
+            .eq(1)
+            .click()
+
+        // wait for validation
+        cy
+            .wait(5000);
+
+        // ensure that the user is redirected to the login page
+        cy
+            .url()
+            .should('eq', 'http://localhost:3000/login');
+    });
 });
+
