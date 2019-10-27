e182d1ce4307b9b7cc059c1f342e2ad5e2006d9a
Weoi En Sng <w.sng2@student.unimelb.edu.au>
2019-10-27 22:52:44 +1100

    Refactored assign_tests, item assigned is no longer dependent on index

--- client/cypress/integration/test_suites/assign_tests.js
+++ client/cypress/integration/test_suites/assign_tests.js
@@ -1,21 +1,33 @@
-// visit login page.
-// sign in using primary user
-// assign to uncle benjamin
-// log out
-// sign into uncl ebenjamin's acc
-// look for elemnt of type assigned to
+// PU = Primary User
+// SU = Secondary USer
 
-describe('Get to the Items Page', function () {
-  it('logs into primary user account using the UI', function () {
+// Tests Assign Item Updates
+// 1) PU assigns item then checks that star is on SU's item card
+// 2) PU unassigns item then checks that star is NOT on SU's item card
 
-    // visit the log in page
-    cy.visit("http://localhost:3000/login");
+// test assignees
+const testSecondaryUser = 'Hugh Jackman'
+const noOne = 'No One'
 
-    // sets up primary user test login
-    const username = "pu@test.com"
-    const password = "password"
+// icon classes
+const assignedIcon = 'assigned-icon'
 
-    // type in a valid email
+// test login usernames and passwords
+const testSecondaryUsername = 'su@test.com'
+const testPrimaryUsername = 'pu@test.com'
+const testPassword = 'password'
+
+// assigned object
+const assignObject = "Magic Runes"
+
+// assigned-to icon index
+const assignedIndex = 1
+
+// button texts
+const assignButtonText = "Assign"
+
+// login custom cypress command
+Cypress.Commands.add('login', (username, pw) => {
     cy
         .get('input[name="email"]')
         .type(username);
@@ -23,7 +35,7 @@ describe('Get to the Items Page', function () {
     // type in an invalid password
     cy
         .get('input[name="password"]')
-        .type(password);
+        .type(pw);
 
     // click on the log in button
     cy
@@ -38,29 +50,54 @@ describe('Get to the Items Page', function () {
     cy
         .url()
         .should('eq', 'http://localhost:3000/items');
+
+})
+
+// logout custom cypress command
+Cypress.Commands.add('logout', () => {
+    // logs out of primary user account
+    cy
+        .get('.dropdown-toggle')
+        .click();
+    cy
+        .get('.dropdown-menu > [href="#"]')
+        .click();
+})
+
+// openAssignToModal custom cypress command
+Cypress.Commands.add('openAssignToModal', () => {
+
+    // click on the 'more info' button for the item to be assigned
+    cy
+        .contains(assignObject).parent('div')
+        .within(()=>{
+            cy.get('Button').click();
+        })
+
+    // click on the assign button
+    cy
+        .contains(assignButtonText).click();
+})
+
+////// 1) PU assign item then check that star is on SU's item card
+describe('Get to the Items Page', function () {
+  it('logs into primary user account using the UI', function () {
+
+    // visit the log in page
+    cy.visit("http://localhost:3000/login");
+
+    // primary user login
+    cy.login(testPrimaryUsername, testPassword);
   })
 })
 
-
+// assign item to secondary user
 describe('Assign', function () {
-    it('opens up assignTo modal', function () {
-        const assignObject = "Magic Runes"
-        const assignButtonText = "Assign"
-
-        // click on the 'more info' button for the item to be assigned
-        cy
-            .contains(assignObject).parent('div')
-            .within(()=>{
-                cy.get('Button').click();
-            })
-
-        // click on the assign button
-        cy
-            .contains(assignButtonText).click();
-    })
-
     it('selects user to assign item to and reflect changes', function () {
-        const testSecondaryUser = 'Hugh Jackman'
+
+        // open up assign dialog
+        cy
+            .openAssignToModal()
 
         // enter the name of the assignee
         cy
@@ -74,6 +111,9 @@ describe('Assign', function () {
         cy
             .contains('Confirm')
             .click();
+    })
+
+    it('reflect changes in "assigned to" label', function () {
 
         // check that the assignee label reflects the change
         cy
@@ -99,43 +139,12 @@ describe('Log out of PU and sign into SU', function () {
             .click()
 
         // logs out of primary user account
-        cy
-            .get('.dropdown-toggle')
-            .click();
-        cy
-            .get('.dropdown-menu > [href="#"]')
-            .click();
+        cy.logout()
         })
 
     it('logs into secondary user account using the UI', function () {
 
-      // sets up secondary user test login
-      const username = "su@test.com"
-      const password = "password"
-
-      // type in a valid email
-      cy
-          .get('input[name="email"]')
-          .type(username);
-
-      // type in a valid password
-      cy
-          .get('input[name="password"]')
-          .type(password);
-
-      // click on the log in button
-      cy
-          .contains('Log In')
-          .click();
-
-      // wait for the next page to load
-      cy
-          .wait(3000);
-
-      // ensure that the page is now the items page
-      cy
-          .url()
-          .should('eq', 'http://localhost:3000/items');
+      cy.login(testSecondaryUsername, testPassword)
     })
 
 })
@@ -144,34 +153,98 @@ describe('Assigned To icon is present on assigned object', function () {
 
     it('checks if star exist on the assigned object', function () {
 
-        const assignObject = "Magic Runes"
-        //go to item card's icon bar
+        // check: first icon is star icon
 
-        cy
-            .contains(assignObject).parent('div')
-            .within(()=>{
-                cy.get('.assigned-icon');
-            })
-
+        cy.contains(assignObject).parent().within(()=> {
+            cy.get('div').eq(assignedIndex)
+            .children().first()
+            .should('have.class', assignedIcon)
+        })
     })
 })
 
 
+describe('Log out of SU and log into PU', function () {
 
+    it('logout', function () {
+        cy.logout()
+    })
 
+    it('logs into primary user account', function () {
+      cy.login(testPrimaryUsername, testPassword)
+    })
 
- // select more info on chosen item card
- // toggle assign to to hugh jackman
- //check that the value of the assign to field changes
-  // log out an dgo into hugh jackman's acc
-  //check that there is a star icon on the item's card
-  //go back to pu
-  //look fo rthe same item
- // unassign hugh jackman
- // go to hugh jack;s account
- // check that star is not there anymore
- // go back to PU
- // select the item check that if no one is selected it reflects no one
- // select another users
- //log into anothe ruser's acc
- // check that assign to star is lit
+})
+
+////// 2) PU unassign item then check that star is NOT on SU's item card
+
+// assign the same item to "No One"
+describe('Assign', function () {
+    it('selects user to assign item to and reflect changes', function () {
+        // open up assign dialog
+        cy
+            .openAssignToModal()
+
+        // enter the name of the assignee
+        cy
+            .get('input.search')
+            .type(noOne);
+
+        // wait for the search results to appear
+        cy.wait(4000)
+
+        // confirm the changes
+        cy
+            .contains('Confirm')
+            .click();
+    })
+
+    it('reflect changes in "assigned to" label', function () {
+        // check that the assignee label reflects the change
+        cy
+            .get('.assignee').should('contain', noOne)
+    })
+})
+
+// log out of PU and login to SU
+describe('Log out of PU and sign into SU', function () {
+
+    it('exit modals and logout', function () {
+
+        // exits "assign" modal
+        cy
+            .get('.assign-modal > .modal-dialog > .modal-content > .modal-header > .close ')
+            .click()
+
+        // waits for the modal to close
+        cy.wait(3000)
+
+        // exits the item modal
+        cy
+            .get('.close ')
+            .click()
+
+        // logs out of primary user account
+        cy.logout()
+        })
+
+    it('logs into secondary user account using the UI', function () {
+
+      cy.login(testSecondaryUsername, testPassword)
+
+    })
+})
+
+// check to see if changes are reflected in the secondary user account
+describe('Assigned To icon is NOT present on un-assigned object', function () {
+
+    it('checks if star exist on the assigned object', function () {
+
+        // check : first icon is not star icon
+        cy.contains(assignObject).parent().within(()=> {
+            cy.get('div').eq(assignedIndex)
+            .children().first()
+            .should('not.have.class', assignedIcon)
+        })
+    })
+})
