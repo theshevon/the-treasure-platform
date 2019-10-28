d9406a315c0c1e20d10f9a8fec7e52bc1cabe364
Shevon Mendis <shevonmendis98@gmail.com>
2019-10-28 16:13:42 +1100

    commented all the components

--- client/src/components/util/Navbar.js
+++ client/src/components/util/Navbar.js
@@ -14,11 +14,15 @@ import { logoutUser } from '../../redux/actions/userActions';
 // custom css
 import '../../stylesheets/navbar.css';
 
-// logos
+// images
 import logo     from '../../icons/logo.svg';
 import logo_alt from '../../icons/logo-alt.svg';
 import no_img   from '../../images/no_img.png';
 
+/**
+ * Represents a responsive navbar that changes in appearance based on whether
+ * or not the page has been scrolled.
+ */
 class NavbarComponent extends Component {
 
     state = {
@@ -26,6 +30,9 @@ class NavbarComponent extends Component {
     }
 
     componentDidMount() {
+
+        // add an event listener to change the appearance of the navbar based
+        // on whether or not the page has been scrolled
         document.addEventListener('scroll', () => {
             const scrolled = window.scrollY !== 0;
             if (scrolled !== this.state.scrolled) {
@@ -34,6 +41,9 @@ class NavbarComponent extends Component {
         });
     }
 
+    /**
+     * Handles the logging out of a user from the platform
+     */
     handleLogout = event => {
         event.preventDefault();
         this.props.logoutUser();
@@ -52,12 +62,14 @@ class NavbarComponent extends Component {
             brandClass  = "navbar-brand navbar-brand-scrolled";
         }
 
+        // by default, there shouldn't be any menu items
         let menuItems = (
             <Nav
                 className="mr-auto">
             </Nav>
         );
 
+        // retrieve the user's data
         let name   = localStorage.TreasureUName || user.name || "Loading User...";
         let imgSrc = localStorage.TreasureUImg  || user.imgSrc || no_img;
         let type   = localStorage.TreasureUType || user.type;
@@ -66,6 +78,7 @@ class NavbarComponent extends Component {
             type = parseInt(type);
         }
 
+        // if the user is a primary user, add the Dashboard and Items menu items
         if (type === 0){
             menuItems = (
                 <Nav
@@ -86,11 +99,13 @@ class NavbarComponent extends Component {
 
             <Container
                 className="container">
+
                 <Navbar
                     className={navbarClass}
                     expand="sm"
                     fixed="top">
 
+                    {/* Treasure Logo and Name */}
                     <Navbar.Brand
                         href="/"
                         className={ brandClass + " d-flex align-items-center"}>
@@ -106,17 +121,22 @@ class NavbarComponent extends Component {
                         </h2>
                     </Navbar.Brand>
 
+                    {/* hamburger for toggling navbar on mobile */}
                     <Navbar.Toggle
                         aria-controls="basic-navbar-nav" />
 
+                    {/* menu items */}
                     <Navbar.Collapse
                         id="basic-navbar-nav"
                         className="mt-2">
 
+                        {/* left-side menu items */}
                         { menuItems }
 
+                        {/* right-side menu items */}
                         <Nav>
 
+                            {/* user image */}
                             <Nav.Link
                                 href="/items">
                                 <img
@@ -128,19 +148,25 @@ class NavbarComponent extends Component {
                                 </img>
                             </Nav.Link>
 
+                            {/* dropdown for additional user options */}
                             <NavDropdown
                                 title={ name }
                                 id="collasible-nav-dropdown">
+
                                 <NavDropdown.Item
                                     href="/support">
                                     Support
                                 </NavDropdown.Item>
+
                                 <NavDropdown.Divider />
+
                                 <NavDropdown.Item
                                     onClick={ this.handleLogout }>
                                     Logout
                                 </NavDropdown.Item>
+
                             </NavDropdown>
+
                         </Nav>
                     </Navbar.Collapse>
 
@@ -151,8 +177,8 @@ class NavbarComponent extends Component {
 }
 
 NavbarComponent.propTypes = {
-    user: PropTypes.object.isRequired,
-    logoutUser: PropTypes.func.isRequired,
+    user       : PropTypes.object.isRequired,
+    logoutUser : PropTypes.func.isRequired,
 }
 
 const mapStatesToProps = (state) => ({
